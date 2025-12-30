6a283cdaa8f4d0061b493bf1272da8a2cae78587
Mubi <mubi@Mubis-MacBook-Air.local>
2025-12-30 15:36:32 +0530

    agc new set of products

--- armer/links.js
+++ armer/links.js
@@ -2,5 +2,7 @@ const allLinks = [
 	"http://amazon.sa/d/B0G53M694S",
 	"http://amazon.sa/d/B0G53PJ9JM",
 	"http://amazon.sa/d/B0G53L6ZXN",
-	"http://amazon.sa/d/B0G53K9VCS"
+	"http://amazon.sa/d/B0G53K9VCS",
+	"https://amazon.sa/d/B0G6Z2TV7P",
+	"https://amazon.sa/d/B0G6Z1G62P"
 ];

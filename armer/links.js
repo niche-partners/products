c1aeed6de11df5d9e87e3190ef12cb2cbda4aa66
Mubi <mubi@Mubis-MacBook-Air.local>
2026-02-09 16:14:46 +0530

    added new links

--- armer/links.js
+++ armer/links.js
@@ -4,5 +4,6 @@ const allLinks = [
 	"http://amazon.sa/d/B0G53L6ZXN",
 	"http://amazon.sa/d/B0G53K9VCS",
 	"https://amazon.sa/d/B0G6Z2TV7P",
-	"https://amazon.sa/d/B0G6Z1G62P"
+	"https://amazon.sa/d/B0G6Z1G62P",
+	"https://amazon.sa/d/B0GDGDMQW1"
 ];
